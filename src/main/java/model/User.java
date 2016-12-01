a1c25b4080ca190d4b56765ed1309a9ecb8d95b8
Artur Nowicki <arturnowicki@gmail.com>
2016-12-01 16:55:02 +0100

    DB initialization
    working initialization with annotated user

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,10 +1,19 @@
 package model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 @Entity
-@Table
+@Table(name = "user", uniqueConstraints = {@UniqueConstraint(columnNames = {"ID"})})
 public class User {
-
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id", length = 11, nullable = false, unique = true)
+	private int id;
+	
 }
